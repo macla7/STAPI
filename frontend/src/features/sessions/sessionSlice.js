7094b4bb9d200e03254068f222ab6b5745cbcf4b
Mitch <MitchelClark1997@outlook.com>
2023-01-29 07:46:02 -0600

    getting API working with updated dependencies

--- frontend/src/features/sessions/sessionSlice.js
+++ frontend/src/features/sessions/sessionSlice.js
@@ -7,8 +7,8 @@ import {
   loginUserWithToken,
 } from "./sessionAPI";
 
-const client_id = "Lm-9XiqY8dZOCoeX3mZrDhCjFY99IAUnp-Y82YpZz_k";
-const client_secret = "m3T95wtJwYXtvWo-1pUI4kpSPSf6BtxL96akVdi5Hjg";
+const client_id = "yj3rroGJVmwir_EkP1XyI_qI2mzN0CI2DJawduMXFUQ";
+const client_secret = "vxGIWxR_RJoESDT9Dn_fiFArwPIz7OFpPpcdhljhuAg";
 
 const initialState = {
   auth_token: null,
