32d42a06852d491ccf9eb0f9f8c35d55d15db9b3
Mitch <MitchelClark1997@outlook.com>
2023-06-29 15:25:38 +1000

    support page

---
+++ app/controllers/support_controller.rb
@@ -0,0 +1,8 @@
+class SupportController < ApplicationController
+  # GET / (root)
+  def how_to
+  end
+
+  def contact_us
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   root 'privacy_policy#home', to: 'privacy_policy#home'
+  get 'support/how_to', to: 'support#how_to'
+  get 'support/contact_us', to: 'support#contact_us'
 
   draw :api
 
